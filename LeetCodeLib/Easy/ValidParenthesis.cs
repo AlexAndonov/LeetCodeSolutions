aa1c4081f285f0d2692b0ce4b90f38cf57a016c8
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2025-09-29 22:29:19 +0300

    Adding Solution for ValidParenthesis problem

---
+++ LeetCodeLib/Easy/ValidParenthesis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLib.Easy
+{
+	public static class ValidParenthesis
+	{
+		public static bool Solve(string s)
+		{
+			Stack<char> stack = new();
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				char c = s[i];
+
+				if (c == '(' || c == '[' || c == '{')
+				{
+					stack.Push(s[i]);
+				}
+				else
+				{
+					if (stack.Count == 0) return false;
+					char top = stack.Pop();
+
+					if (c == ')' && top != '(') return false;
+					if (c == ']' && top != '[') return false;
+					if (c == '}' && top != '{') return false;
+				}
+			}
+
+			return stack.Count == 0;
+		}
+	}
+}
