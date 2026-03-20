96bd235e5551bb26a786023825abc8b9dc8455ad
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-03-20 16:18:47 +0200

    Solve Merge Alternately

---
+++ LeetCodeLib/LeetCode75/Easy/MergeAlternately.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLib.LeetCode75.Easy
+{
+    public static class MergeAlternately
+    {
+        public static string Solve(string word1, string word2)
+        {
+            StringBuilder result = new StringBuilder();
+
+            int i = 0;
+            while (i < word1.Length || i < word2.Length)
+            {
+                if (i < word1.Length)
+                {
+                    result.Append(word1[i]);
+                }
+
+                if (i < word2.Length)
+                {
+                    result.Append(word2[i]);
+                }
+
+                i++;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCodeTests/LeetCode75Tests/Easy/MergeAlternatelyTests.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeLib.LeetCode75.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests.LeetCode75Tests.Easy
+{
+    public class MergeAlternatelyTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string word1 = "abc";
+            string word2 = "pqr";
+
+            string expected = "apbqcr";
+            string actual = MergeAlternately.Solve(word1, word2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string word1 = "ab";
+            string word2 = "pqrs";
+
+            string expected = "apbqrs";
+            string actual = MergeAlternately.Solve(word1, word2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string word1 = "abcd";
+            string word2 = "pq";
+
+            string expected = "apbqcd";
+            string actual = MergeAlternately.Solve(word1, word2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
