3f3243342894026728a7d819506d7b263e0c7cd3
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-03-20 16:16:53 +0200

    Solve Unique Nmber of Occurences

---
+++ LeetCodeLib/LeetCode75/Easy/UniqueNumberOfOccurences.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLib.LeetCode75.Easy
+{
+    public static class UniqueNumberOfOccurences
+    {
+        public static bool Solve(int[] arr)
+        {
+            Dictionary<int, int> occurences = new Dictionary<int, int>();
+
+            foreach (int num in arr)
+            {
+                if (occurences.ContainsKey(num))
+                {
+                    occurences[num]++;
+                }
+                else
+                {
+                    occurences[num] = 1;
+                }
+            }
+
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach (int i in occurences.Values)
+            {
+                if (!seen.Add(i))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTests/LeetCode75Tests/Easy/UniqueNumberOfOccurencesTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeLib.LeetCode75.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests.LeetCode75Tests.Easy
+{
+    public class UniqueNumberOfOccurencesTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] arr = [1, 2, 2, 1, 1, 3];
+            bool expected = true;
+            bool actual = UniqueNumberOfOccurences.Solve(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] arr = [1, 2];
+            bool expected = false;
+            bool actual = UniqueNumberOfOccurences.Solve(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0];
+            bool expected = true;
+            bool actual = UniqueNumberOfOccurences.Solve(arr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
